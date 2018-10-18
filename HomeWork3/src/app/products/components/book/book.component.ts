8c277b65f16a829aea929df75adb9561231b7f09
AndriBunko <andr1bunk02@gmail.com>
2018-10-18 14:46:57 +0300

    Homework 4 task 1.7.1

--- HomeWork3/src/app/products/components/book/book.component.ts
+++ HomeWork3/src/app/products/components/book/book.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {BookModel} from "../../models/book.model";
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  // styleUrls: ['./book.component.css']
+  styles: [`
+    p {
+      border: 2px solid red;
+      background-color: dodgerblue;
+      text-align: center;
+      margin-bottom: 20px;
+    }
+
+    .homework {
+      border: 2px solid red;
+      background-color: yellow;
+      text-align: center;
+      margin-bottom: 20px;
+      font-family: "Comic Sans MS";
+      font-size: 20px;
+    }`]
+
 })
 export class BookComponent implements OnInit {
+  @Input()
+  book: BookModel;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
